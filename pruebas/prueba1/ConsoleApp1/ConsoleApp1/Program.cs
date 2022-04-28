7f59ac5c28d02c42c62e34dc7a9e950ba21cc45c
fabygithub <36684138+fabygithub@users.noreply.github.com>
2022-04-28 11:09:27 -0400

    Add files via upload

---
+++ pruebas/prueba1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            // Variables
+            string dato = "";
+          
+            string nombre="";
+
+
+            int ancho1=0;
+            int alto1=0;
+            int area1=0;
+
+            int ancho2=0;
+            int alto2=0;
+            int area2=0;
+
+            int areatotal = 0;
+
+            // Solicitamos los valores al usuario
+
+            Console.WriteLine("Buenas tardes");
+            Console.WriteLine("Ingrese nombre: ");
+            dato = Console.ReadLine();
+            nombre = (dato);
+
+
+            Console.WriteLine("Ingrese 1 habitacion: ");
+
+            Console.WriteLine("Ingrese ancho: ");
+            dato = Console.ReadLine();
+            ancho1 = Convert.ToInt32(dato);
+
+            Console.WriteLine("Ingrese alto ");
+            dato = Console.ReadLine();
+            alto1 = Convert.ToInt32(dato);
+
+            Console.WriteLine("el area es : ");
+            area1 = ancho1 * alto1;
+
+
+            Console.WriteLine("Ingrese 2 habitacion: ");
+
+            Console.WriteLine("Ingrese ancho: ");
+            dato = Console.ReadLine();
+            ancho2 = Convert.ToInt32(dato);
+
+            Console.WriteLine("Ingrese alto ");
+            dato = Console.ReadLine();
+            alto2 = Convert.ToInt32(dato);
+
+            Console.WriteLine("el area es : ");
+            area2 = ancho2 * alto2;
+
+
+            aretotal = area1 + area2;
+
+
+            Console.WriteLine("el area de habitacion 1 es {0}: ", area1);
+
+            Console.WriteLine("el area de habitacion 2 es {0}: ", area2);
+
+            Console.WriteLine("el area total es {0}: ", areatotal);
+
+
+
+
+        }
+    }
+}
---
+++ pruebas/prueba1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Variables
+            string dato = "";
+
+
+
+            string nombre="";
+            string apellido="";
+
+
+
+            int opcion = 0;
+            double cantidad = 0;
+            double dolar = 885.88;
+            double euro = 896.75;
+            double valor = 0;
+
+            // Solicitamos los valores al usuario
+
+            Console.WriteLine("Buenas tardes");
+            Console.WriteLine("Ingrese nombre: ");
+            dato = Console.ReadLine();
+            nombre = (dato);
+
+
+            Console.WriteLine("Ingrese apellido: ");
+            dato = Console.ReadLine();
+            apellido = (dato);
+
+
+            Console.WriteLine("Ingrese valor en pesos");
+            dato = Console.ReadLine();
+            valor = Convert.ToDouble(dato);
+
+
+
+            Console.WriteLine("elegir opcion 1 dolar 2 euros");
+            dato = Console.ReadLine();
+            opcion = Convert.ToInt32(dato);
+
+
+            switch (opcion)
+            {
+                case 1:
+
+                    cantidad = valor / dolar;
+
+                    Console.WriteLine("el resultado es para {0}: {1} la conversion de pesos a divisa {2} ", nombre, apellido, cantidad);
+
+
+                    break;
+                case 2:
+
+
+                    cantidad = valor / euro;
+
+                    Console.WriteLine("el resultado es para {0}: {1} la conversion de pesos a divisa {2} ", nombre, apellido, cantidad);
+
+                    break;
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
+
